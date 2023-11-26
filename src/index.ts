d9e999e1e5523c9655d648cf312bbfe2179da479
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-11-26 17:18:56 -0300

    basic crud types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-console.log("auto update")
+const nameList: string[] = [];
 
-/*BASIC TYPES
-Por ser tipado o typescript consegue assumir qual é o tipo da variável pelo valor dela, seja boolean,number e etc...
+const addName = (name: string) => {
+    nameList.push(name)
+}
 
+const readNames = () =>{
+    nameList.forEach((nameUser) => {
+        console.log(nameUser)
+    })
+}
 
-Tipo any-ex:
-let dynamicValue: any;
-dynamicValue = 42; // number
-dynamicValue = "hello"; // string
-dynamicValue = true; // boolean
+const removeNameList = (i: number) =>{
+    nameList.splice(i, 1)
+}
+const updateNameList = (i: number, name: string) =>{
+    nameList.splice(i, 1, name)
+}
 
-o any nesse caso foi usado como exmplo por não termos certeza de qual é o tipo da variável ou para permitir qualquer tipo de valor
-*/
+addName("lucas")
+addName("samuel")
+addName("luisa")
 
+removeNameList(1)
+
+updateNameList(1, "luciana")
+
+readNames()
