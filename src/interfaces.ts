5c106c6101674432af9ef2d914c1a111d92277b5
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-11-26 17:46:57 -0300

    Basic interfaces use

---
+++ src/interfaces.ts
@@ -0,0 +1,9 @@
+interface IUser{
+    id: number;
+    name: string;
+    email: string;
+    module: string;
+}
+
+
+export const user: IUser = {id:2, name: "Lucas", email: "lucas@gmail.com.br", module: "modulo 4"}
