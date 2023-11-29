0dbc341bd7f423ddae62c43fc83cf150da9dfa64
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-11-29 20:17:55 -0300

    class intro

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-console.log("auto update")
+class Person {
+    name: string = "Lucas"
+    surname: string ="Dias"
 
-/*BASIC TYPES
-Por ser tipado o typescript consegue assumir qual é o tipo da variável pelo valor dela, seja boolean,number e etc...
+    profission(message:string){
+        console.log(message)
+    }
+}
 
+const person = new Person();
+console.log(person.name + " " + person.surname)
 
-Tipo any-ex:
-let dynamicValue: any;
-dynamicValue = 42; // number
-dynamicValue = "hello"; // string
-dynamicValue = true; // boolean
-
-o any nesse caso foi usado como exmplo por não termos certeza de qual é o tipo da variável ou para permitir qualquer tipo de valor
-*/
-
+person.profission("Web Developer")
