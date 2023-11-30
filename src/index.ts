ab97bece13e99a49f4685fda400ad3cd0778d125
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-11-29 21:31:00 -0300

    activity create student class

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,36 @@ const newPerson = new Person2("lucas");
 
 newPerson.sayProfission("Full stack developer");
 newPerson.sayMyname();
+
+class Student{
+    name: string = "";
+    module: string = "";
+    grade: number | null = null
+
+    constructor(name: string, module: string){
+        this.name = name
+        this.module = module
+    }
+
+    studentApresentation(){
+        console.log(`Olá pessoal meu nome é ${this.name} e estou no ${this.module}`)
+    }
+
+    setGrade(note: number){
+        this.grade = note
+    }
+
+    getGrade(){
+        console.log(this.grade)
+    }
+}
+
+const studentNameAndModule = new Student("Osvaldo", "M4")
+
+studentNameAndModule.studentApresentation()
+studentNameAndModule.setGrade(20)
+studentNameAndModule.getGrade()
+
+
+
+
